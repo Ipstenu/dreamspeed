c70d0ee926a3798d86c58fe43c7656b9c71275a6
Ipstenu <ipstenu@ipstenu.org>
2014-05-01 15:24:59 -0700

    poco update

--- vendor/aws/aws-autoloader.php
+++ vendor/aws/aws-autoloader.php
@@ -14,7 +14,9 @@
  * permissions and limitations under the License.
  */
 
-include_once( __DIR__ . '/Symfony/Component/ClassLoader/UniversalClassLoader.php' );
+if ( !class_exists('UniversalClassLoader') ) {
+	require_once __DIR__ . '/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
 
 if (!defined('AWS_FILE_PREFIX')) {
     define('AWS_FILE_PREFIX', __DIR__);
